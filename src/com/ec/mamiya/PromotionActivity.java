3dcc19781563ceaab3c7429fcdc50395fa3c419c
striversist <striversist@qq.com>
2014-11-27 22:42:24 +0800

    [home]简单的网页版面

--- src/com/ec/mamiya/PromotionActivity.java
+++ src/com/ec/mamiya/PromotionActivity.java
@@ -1,22 +1,43 @@
 package com.ec.mamiya;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class PromotionActivity extends Activity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promotion);
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.loadUrl("http://m.vip.com");
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new HelloWebViewClient());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.promotion, menu);
-        return true;
+    // 设置回退
+    // 覆盖Activity类的onKeyDown(int keyCoder,KeyEvent event)方法
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack(); // goBack()表示返回WebView的上一页面
+            return true;
+        }
+        return false;
     }
 
+    // Web视图
+    private class HelloWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
 }
