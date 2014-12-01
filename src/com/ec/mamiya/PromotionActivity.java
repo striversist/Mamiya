b3d35c1e9825246810c1f0fa6b516aa342b4ecc2
striversist <striversist@qq.com>
2014-12-01 22:11:58 +0800

    [promotion]UI:增加顶部操作栏

--- src/com/ec/mamiya/PromotionActivity.java
+++ src/com/ec/mamiya/PromotionActivity.java
@@ -2,42 +2,23 @@ package com.ec.mamiya;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.View;
 
 public class PromotionActivity extends Activity {
-
-    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promotion);
-
-        mWebView = (WebView) findViewById(R.id.webview);
-        mWebView.loadUrl("http://m.vip.com");
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new HelloWebViewClient());
+        
+        
     }
-
-    @Override
-    // 设置回退
-    // 覆盖Activity类的onKeyDown(int keyCoder,KeyEvent event)方法
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
-            mWebView.goBack(); // goBack()表示返回WebView的上一页面
-            return true;
-        }
-        return false;
+    
+    public void onMenuClicked(View view) {
+        
     }
-
-    // Web视图
-    private class HelloWebViewClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
+    
+    public void onSearchClicked(View view) {
+        
     }
 }
