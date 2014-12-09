9a02f03aad753456021672be9bef2b56cf8559d0
striversist <striversist@qq.com>
2014-12-09 23:09:29 +0800

    [product]UI:产品详细页（初步）

---
+++ src/com/ec/mamiya/ProductActivity.java
@@ -0,0 +1,13 @@
+package com.ec.mamiya;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProductActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+}
