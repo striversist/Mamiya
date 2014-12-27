bc6e255f63c6e78f46de6c8836b31a0d33c4e5d9
striversist <striversist@qq.com>
2014-12-27 17:04:23 +0800

    [product]UI：增加“商品评价”列表中的布局

---
+++ src/com/ec/mamiya/view/ProductSatisfactionView.java
@@ -0,0 +1,21 @@
+package com.ec.mamiya.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class ProductSatisfactionView extends LinearLayout {
+
+    public ProductSatisfactionView(Context context) {
+        this(context, null, 0);
+    }
+    
+    public ProductSatisfactionView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+    
+    public ProductSatisfactionView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+}
